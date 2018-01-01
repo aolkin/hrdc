bbcc021b58cb5faf6d45b236bc7ca50f0f05a97e
Aaron Olkin <aaronolkin@gmail.com>
2018-01-01 02:04:54 -0500

    Buxfixes to emailtracker

--- emailtracker/tasks.py
+++ emailtracker/tasks.py
@@ -2,6 +2,10 @@ from celery import shared_task
 
 from importlib import import_module
 
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
 @shared_task(acks_late=True, ignore_result=True)
 def send_queued(pk):
     email = import_module('emailtracker.models').QueuedEmail.objects.get(pk=pk)
@@ -11,4 +15,4 @@ def send_queued(pk):
 def send_missing():
     n = import_module('emailtracker.tools').reschedule_all()
     if n:
-        print("{} unset emails rescheduled.".format(n))
+        LOGGER.warn("{} unsent emails rescheduled.".format(n))
