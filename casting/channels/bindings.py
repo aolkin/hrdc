2ee5ee1c5273cfe42e6d06aca7e23f650d66a4e1
Aaron Olkin <aaronolkin@gmail.com>
2018-01-13 17:36:39 -0500

    Allow hiding characters during casting

--- casting/channels/bindings.py
+++ casting/channels/bindings.py
@@ -30,7 +30,8 @@ class AssociatedShowBinding(WebsocketBinding):
 class CharacterBinding(AssociatedShowBinding):
     model = Character
     stream = "character"
-    fields = ("name", "callback_description", "allowed_signers")
+    fields = ("name", "callback_description", "allowed_signers",
+              "hidden_for_signing")
 
     @classmethod
     def group_names(cls, instance):
@@ -59,6 +60,8 @@ class CharacterBinding(AssociatedShowBinding):
         obj = self.model.objects.get(pk=pk)
         if "allowed_signers" in data and obj.show.first_cast_submitted:
                 data["allowed_signers"] = obj.allowed_signers
+        if "hidden_for_signing" in data and data["hidden_for_signing"]:
+            obj.signing_set.all().delete()
         if obj.show.callbacks_submitted and not obj.added_for_signing:
             if "name" in data:
                 del data["name"]
