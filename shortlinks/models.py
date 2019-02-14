8e88a943e635e232f77342f46feeaf88b2e660eb
Aaron Olkin <aaronolkin@gmail.com>
2019-02-13 22:02:26 -0500

    Made it prettier

--- shortlinks/models.py
+++ shortlinks/models.py
@@ -11,18 +11,22 @@ class Link(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "/" + self.url
     
     @property
     def link(self):
         try:
             return settings.SHORTLINK_PREFIX + self.url
         except AttributeError:
-            return reverse("shortlink", kwargs={"slug": self.url})
+            return settings.SITE_URL.partition("://")[2] + reverse(
+                "shortlink", kwargs={"slug": self.url})
 
     def link_markup(self):
         return mark_safe(
-            '<a href="{0}" target="blank">{0}</a>'.format(self.link))
+            '<a href="http://{0}" target="blank">{0}</a>'.format(self.link))
     link_markup.short_description = "Shortlink"
         
     def get_absolute_url(self):
-        return self.link
+        return "http://" + self.link
