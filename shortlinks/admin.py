90a8725b5d6e992d0249197860bd21579a4e3045
Aaron Olkin <aaronolkin@gmail.com>
2019-02-13 22:16:12 -0500

    Made it prettier

--- shortlinks/admin.py
+++ shortlinks/admin.py
@@ -4,7 +4,7 @@ from .models import Link
 
 @admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
-    list_display = ("url", "destination", "link_markup")
+    list_display = ("__str__", "destination", "link_markup")
     list_filter = ("created", "updated")
 
     readonly_fields = ("created", "updated", "link")
