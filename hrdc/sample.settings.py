c40a8df8e4f350c868cfcfd20bfb0a6fad76b576
Aaron Olkin <aaronolkin@gmail.com>
2019-08-05 23:57:02 -0400

    Fix creation and login up pipeline

--- hrdc/sample.settings.py
+++ hrdc/sample.settings.py
@@ -101,6 +101,18 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'INSERT_PROVIDED_SECRET_HERE'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'dramaorg.utils.social_create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'social_core.pipeline.social_auth.associate_by_email',
+)
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
