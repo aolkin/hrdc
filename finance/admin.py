bb6babc6ef3c9c4e5112c7d6b5ab40f5b5b5ae50
Aaron Olkin <aaronolkin@gmail.com>
2019-09-11 18:42:13 -0400

    Added income

--- finance/admin.py
+++ finance/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 
 from .models import *
 
+class IncomeAdmin(admin.TabularInline):
+    model = Income
+    extra = 1
+    fields = (
+        ("name", "status"),
+        ("amount_requested", "amount_received"),
+    )
+
 @admin.register(FinanceInfo)
 class MetaAdmin(admin.ModelAdmin):
-    list_display = ('show', 'season',)
+    list_display = ('show', 'season', "income_count",
+                    "planned_income", "received_income")
     search_fields = ('show__title',)
     list_filter = ('show__season', 'show__year',)
     autocomplete_fields = ('show',)
@@ -13,6 +22,12 @@ class MetaAdmin(admin.ModelAdmin):
             "fields": ('show',)
         }),
     )
+
+    inlines = (IncomeAdmin,)
     
     def season(self, obj):
         return obj.show.seasonstr()
+
+    def income_count(self, obj):
+        return obj.income_set.count()
+    income_count.short_description = "# of Grants"
--- finance/models.py
+++ finance/models.py
@@ -14,6 +14,46 @@ class FinanceInfo(models.Model):
 
     class Meta:
         verbose_name = "Finance-Enabled Show"
-    
+
+    def planned_income(self):
+        sum = self.income_set.exclude(status=11).aggregate(
+            models.Sum("amount_requested"))["amount_requested__sum"]
+        return "${:.2f}".format(sum) if sum else sum
+        
+    def received_income(self):
+        sum = self.income_set.filter(status__gt=90).aggregate(
+            models.Sum("amount_received"))["amount_received__sum"]
+        return "${:.2f}".format(sum) if sum else sum
+        
     def __str__(self):
         return str(self.show)
+
+class Income(models.Model):
+    INCOME_STATUSES = (
+        (0, "Planned"),
+        (1, "Applied"),
+        (10, "Confirmed"),
+        (11, "Rejected"),
+        (
+            "Administrative Statuses", (
+                (91, "Funds Received"),
+                (92, "A.R.T. Grant"),
+            )
+        )
+    )
+    
+    show = models.ForeignKey(FinanceInfo, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+
+    amount_requested = models.DecimalField(decimal_places=2, max_digits=7)
+    amount_received = models.DecimalField(decimal_places=2, max_digits=7,
+                                          null=True, blank=True)
+
+    status = models.PositiveSmallIntegerField(choices=INCOME_STATUSES,
+                                              default=0)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Grant/Income"
