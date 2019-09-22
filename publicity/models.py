c3dbf1a493ce2af44650f3c2c33b0b086e7f9c60
Aaron Olkin <aaronolkin@gmail.com>
2019-09-22 01:36:21 -0400

    Updated message

--- publicity/models.py
+++ publicity/models.py
@@ -117,7 +117,8 @@ class Announcement(models.Model):
     title = models.CharField(max_length=100)
     message = models.TextField()
     graphic = models.ImageField(
-        blank=True, help_text="Optional graphic to include with your message.",
+        blank=True, help_text="Optional graphic to include with your message. "
+        "Graphics may be ommitted at the editor's discretion.",
         upload_to='publicity/announcements/%Y/%m/%d/')
     note = models.TextField(blank=True, verbose_name="Note for the Editor",
                             help_text="This will not be published.")
