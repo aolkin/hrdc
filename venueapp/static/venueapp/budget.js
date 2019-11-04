83b0999a054dbf0f41a0f8ff7a262d3dd2207c5d
Aaron Olkin <aaronolkin@gmail.com>
2019-11-04 17:43:48 -0500

    Made budget a spreadsheet

--- venueapp/static/venueapp/budget.js
+++ venueapp/static/venueapp/budget.js
@@ -7,13 +7,30 @@ $(() => {
     }
     $(".budget-line").on("click", ".remove-line", deleteLine);
 
+    let formatter = new Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'USD',
+    });
+
+    function recalculate() {
+	let net = 0;
+	$(".budget-line .dollar-field input").each((index, el) => {
+	    let multiplier = $(el).parents(".dollar-field").hasClass(
+		"category-income") ? 1 : -1;
+	    net += $(el).val() * multiplier;
+	});
+	$("#net-total").val(formatter.format(net));
+    }
+
     $(".dollar-field input").each((index, el) => {
 	let $el = $(el);
 	let prepend = $("<div>").addClass("input-group-prepend");
 	prepend.append('<span class="input-group-text">$</span>');
 	$el.parent().addClass("input-group");
 	$el.parent().prepend(prepend);
-    });
+	$el.addClass("text-right");
+    }).on("input", recalculate);
+    recalculate();
 
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
 	$(".add-budget-venue input").val($(this).data("venue"));
