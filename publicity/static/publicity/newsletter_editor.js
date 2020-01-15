f2c39a2c871e53cc563bb1a02035588047dc70ac
Aaron Olkin <aaronolkin@gmail.com>
2020-01-15 16:58:35 -0500

    Added missing file

---
+++ publicity/static/publicity/newsletter_editor.js
@@ -0,0 +1,22 @@
+$(() => {
+    
+    let input = $("textarea#id_message").hide();
+    let editor = $("<div>").html(input.val()).insertAfter(input);
+    let toolbar = $("<div>").insertAfter(input);
+
+    var quill = new Quill(editor[0], {
+	modules: {
+	    toolbar: [
+		['bold', 'italic'],
+		[{ 'list': 'ordered'}, { 'list': 'bullet' }],
+		['link'],
+		['clean'],
+	    ]
+	},
+	placeholder: "Compose your message body here...",
+	theme: 'snow'
+    });
+    quill.on('text-change', () => {
+        input.val(editor.children(".ql-editor").html());
+    });
+});
