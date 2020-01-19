b06dbb8063fb1475417c1e5d465277831e3d67cc
Aaron Olkin <aaronolkin@gmail.com>
2020-01-18 20:31:56 -0500

    Added geolocation error messages

--- casting/static/casting/sign_in_public.js
+++ casting/static/casting/sign_in_public.js
@@ -10,10 +10,25 @@ $(function(){
         $form.submit();
     }
 
+    function noLocation(error) {
+	$("p.intro").hide();
+        let el = $(".alert-danger").removeClass("d-none");
+	switch(error.code) {
+	    case error.PERMISSION_DENIED:
+		el.text("Location access denied. Please allow access and try again, or sign in at the table.")
+		break;
+	    case error.TIMEOUT:
+		el.find(".message").text("Attempting to determine your location took too long.");
+		break;
+	}
+    }
+
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(redirect);
+        navigator.geolocation.getCurrentPosition(redirect, noLocation, {
+	    timeout: 5000,
+	});
     } else {
-        $(".lead").hide();
-        $(".alert-danger").show();
+        $("p.intro").hide();
+        $(".alert-danger").removeClass("d-none");
     }
 });
