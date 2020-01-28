2ad69b24206c1b178025759d5befcc958a27a29b
Aaron Olkin <aaronolkin@gmail.com>
2020-01-28 18:39:48 -0500

    Add GA events to location sign in

--- casting/static/casting/sign_in_public.js
+++ casting/static/casting/sign_in_public.js
@@ -1,5 +1,11 @@
 $(function(){
     function redirect(pos) {
+	if (window.gtag) {
+	    gtag('event', "location_found", {
+		'event_category': "casting",
+		'event_label': "Location Found"
+	    });
+	}
         $form = $("form.geolocation-form");
         $form.children("input").each(function(index, el) {
             let val = pos.coords[$(el).attr("name")];
@@ -20,6 +26,17 @@ $(function(){
 	    case error.TIMEOUT:
 		el.find(".message").text("Attempting to determine your location took too long.");
 		break;
+	}
+	if (window.gtag) {
+	    let CODES = {};
+	    CODES[error.PERMISSION_DENIED] = "PERMISSION_DENIED";
+	    CODES[error.TIMEOUT] = "TIMEOUT";
+	    CODES[error.POSITION_UNAVAILABLE] = "POSITION_UNAVAILABLE";
+	    gtag('event', "location_error", {
+		'event_category': "casting",
+		'event_label': "Location Error",
+		'value': CODES[error.code],
+	    });
 	}
     }
 
