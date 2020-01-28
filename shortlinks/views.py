f901531b9e630f20e4b23e421846ca8cab8d513e
Aaron Olkin <aaronolkin@gmail.com>
2020-01-28 02:11:06 -0500

    Add utm_campaign

--- shortlinks/views.py
+++ shortlinks/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
+import urllib.parse
+
 from dramaorg.mixins import UserIsPdsmMixin
 
 from config import config
@@ -15,7 +17,10 @@ def link(request, slug):
     l = get_object_or_404(Link, url=slug)
     if config.add_utm_to_shortlinks == "yes":
         sep = "&" if "?" in l.destination else "?"
-        return redirect(l.destination + sep + "utm_medium=referral&utm_source=shortlink")
+        return redirect(
+            l.destination + sep +
+            "utm_medium=referral&utm_source=shortlink&utm_campaign={}".format(
+                urllib.parse.quote_plus(l.url)))
     else:
         return redirect(l.destination)
 
