28f05d29b837f2d23aebb2ee9c76583addefbc03
Aaron Olkin <aaronolkin@gmail.com>
2020-02-04 14:29:39 -0500

    Fix bug for extra added auditions

--- casting/models.py
+++ casting/models.py
@@ -358,14 +358,15 @@ def send_auditions(sender, instance, *args, **kwargs):
                 "html": render_to_string(template, { "audition": instance })
             }
         if instance.status == Audition.STATUSES[1][0]:
-            for aud in Audition.objects.filter(
-                    actor_id=instance.actor_id,
-                    space__building=instance.space.building,
-                    signed_in__date=instance.signed_in).exclude(
-                        id=instance.id).exclude(
-                            status=Audition.STATUSES[1][0]):
-                aud.busy = instance
-                aud.save()
+            if instance.space:
+                for aud in Audition.objects.filter(
+                        actor_id=instance.actor_id,
+                        space__building=instance.space.building,
+                        signed_in__date=instance.signed_in).exclude(
+                            id=instance.id).exclude(
+                                status=Audition.STATUSES[1][0]):
+                    aud.busy = instance
+                    aud.save()
         else:
             for aud in Audition.objects.filter(actor_id=instance.actor_id,
                                                busy=instance):
