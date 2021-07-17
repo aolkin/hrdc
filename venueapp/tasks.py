cd70d4141faf0496f34659de99901cf9edff4e69
Aaron Olkin <aaronolkin@gmail.com>
2021-07-17 16:41:42 -0400

    Fixes #46, attachment merge and bookmark locations for PDF application rendering

--- venueapp/tasks.py
+++ venueapp/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 
 from importlib import import_module
+import traceback as tb
 
 from django.template.loader import render_to_string
 from django.contrib.staticfiles import finders
@@ -8,7 +9,7 @@ from django.db.models import Q
 from django.conf import settings
 
 from weasyprint import HTML, CSS
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfFileMerger, PdfFileReader
 from io import BytesIO
 
 from emailtracker.tools import render_msg
@@ -18,6 +19,17 @@ from config import config
 import logging
 
 LOGGER = logging.getLogger(__name__)
+
+class _Bookmark:
+    def __init__(self, bm):
+        self.label = bm.label
+        self.location = bm.destination[0]
+
+    def __str__(self):
+        return "{} @ {}".format(self.label, self.location)
+
+    def __repr__(self):
+        return "<_Bookmark: {}>".format(self)
 
 @shared_task(acks_late=True, ignore_result=True)
 def render_and_send_app(pk):
@@ -31,28 +43,38 @@ def render_and_send_app(pk):
             "pdf": True, "max_attachment_size": max_bytes,
         })
         doc = HTML(string=html, base_url=settings.SITE_URL).render()
-        bookmarks = doc.make_bookmark_tree()
+        bookmark_tree = doc.make_bookmark_tree()
+        bookmarks = list([_Bookmark(i) for i in bookmark_tree])
         app_pdf = BytesIO()
         doc.write_pdf(app_pdf)
         merger = PdfFileMerger()
-        merger.append(app_pdf)
+        merger.append(app_pdf, import_bookmarks=False)
         for staff in app.staffmember_set.signed_on().filter(
                 role__accepts_attachment=True).exclude(
                     Q(attachment=None) | Q(attachment="")):
             name = "{} {}'s ".format(staff.role_name, staff.person)
             try:
                 if staff.attachment.size < max_bytes:
+                    reader = PdfFileReader(staff.attachment.open(), False)
+                    attachment_pages = reader.getNumPages()
                     page = None
                     for i, bookmark in enumerate(bookmarks):
                         if bookmark.label == name + "Supplement":
-                            page = bookmarks[i + 1].destination[0]
+                            page = bookmarks[i + 1].location
                     if page:
-                        merger.merge(page, staff.attachment.open())
+                        merger.merge(page, staff.attachment.open(),
+                                     import_bookmarks=False)
+                        for i in bookmarks:
+                            if i.location >= page:
+                                i.location += attachment_pages
                     else:
                         merger.append(staff.attachment.open(),
-                                      bookmark=name + "Attachment")
-            except Exception:
-                pass
+                                      bookmark=name + "Attachment",
+                                      import_bookmarks=False)
+            except Exception as e:
+                tb.print_exc()
+        for i in bookmarks:
+            merger.addBookmark(i.label, i.location)
         pdf = BytesIO() # open("/tmp/{}.pdf".format(venue.venue), "wb")
         merger.write(pdf)
         msg = render_msg(
@@ -72,5 +94,6 @@ def render_and_send_app(pk):
         except Exception as err:
             LOGGER.error("Application submission sending failed: {}".format(
                 repr(err)))
+            tb.print_exc()
         finally:
             merger.close()
